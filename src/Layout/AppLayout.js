f4edecfe2d73f416e87eb4befd3930e03b2467c6
rakeshdadi <dadirakeshreddy@gmail.com>
2019-02-20 09:14:56 -0500

    Update AppLayout.js
    removed unused file

--- src/Layout/AppLayout.js
+++ src/Layout/AppLayout.js
@@ -2,7 +2,6 @@ import React from 'react';
 import styled from 'styled-components';
 import { Link } from "react-router-dom";
 import { Layout, Icon } from 'antd';
-import '../App.css';
 
 const { Header, Footer, Content } = Layout;
 
